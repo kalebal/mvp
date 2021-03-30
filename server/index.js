9452f0643fa5db273911e9a0cb23af774fe57f7c
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 15:06:56 -0400

    connect to db on server start

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const {connect, db} = require('../db')
+const { getParks, add, addPark } = require('./controllers/park');
+const dbConnection = require('../db/index');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('/parks', getParks);
+app.post('/parks', addPark);
+app.put('/parks', add);
+
 const PORT = process.env.PORT || 3000;
+
+dbConnection.connect((err, client) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to db!');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Web server running on: http://localhost:${PORT}`);
-  connect();
 });
