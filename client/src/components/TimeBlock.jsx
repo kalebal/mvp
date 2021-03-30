4bf2c63c7fc4e8e6f7e629300e1df129416c9800
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 17:30:08 -0400

    Disable button once clicked

--- client/src/components/TimeBlock.jsx
+++ client/src/components/TimeBlock.jsx
@@ -3,12 +3,18 @@ import React, { useState, useEffect } from 'react';
 
 export default function TimeBlock({ time, onClick }) {
   const [count, setCount] = useState(0);
+  const [showButton, toggleButton] = useState(true);
+  let timeText = `${time}:00 - ${time + 1}:00`;
+
 
   return (
     <li key={time}>
       <div className="hourContainer">
-      {time}
-      <button onClick={() => {
+      {timeText}
+      <button
+      disabled={!showButton}
+      onClick={() => {
+        toggleButton();
         onClick();
         setCount(count + 1); }}> + </button>
       </div>
