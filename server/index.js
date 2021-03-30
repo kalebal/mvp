419f32769e4e3da46b3a43183ad89c41e1f484bf
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 11:31:14 -0400

    Initialize database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const {connect, db} = require('../db')
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,4 +11,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Web server running on: http://localhost:${PORT}`);
+  connect();
 });
