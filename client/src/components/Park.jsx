31f92c0af5589ea81c8cf2daea972959aaca5d6b
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 16:46:20 -0400

    add child event listener

--- client/src/components/Park.jsx
+++ client/src/components/Park.jsx
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import TimeList from './TimeList.jsx';
+import TimeBlock from './TimeBlock.jsx';
 
 export default function Park({ data }) {
-  console.log(data);
+  const [count, setCount] = useState(0);
+
+  let times = [];
+
+  for (var i = parseInt(data.openTime); i < 12 + parseInt(data.closeTime); i++) {
+    times.push(`${i}:00 - ${i + 1}:00`);
+  }
+
+
   return (
-    <>
+    <div className="parkContainer">
     <h3>{data.name}</h3>
-    <TimeList/>
-    </>
+    <h5>Today's Potential Friends: {count}</h5>
+      <ul>
+        {times.map((time) => {
+          return <TimeBlock time={time} onClick={() => setCount(count + 1)}/>;
+        })}
+      </ul>
+    </div>
   )
 }
