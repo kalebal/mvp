9d93e076cd04d352b44c8276778d677d134255b4
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 11:30:54 -0400

    set up database conneciton

---
+++ db/index.js
@@ -0,0 +1,23 @@
+const mongo = require('mongodb');
+
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/dogparks";
+
+let db = '';
+
+let connect = () => {
+    MongoClient.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true },
+    function(err, database) {
+    if (err) {
+      throw err;
+    }
+    db = database;
+    console.log('connected to db!');
+  });
+}
+module.exports = {
+  connect: connect,
+  db: db
+}
