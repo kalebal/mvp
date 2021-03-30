4559f51b49045a5b3a3999ba20933b0d91849314
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-30 16:45:27 -0400

    Removed ref to timeList

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,40 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import TimeList from './TimeList.jsx';
 import Park from './Park.jsx';
 import axios from 'axios';
-
-// class App extends React.Component {
-//   constructor(){
-//     super();
-//     this.state = {parkData: []}
-//   }
-
-//   componentDidMount() {
-//     axios('http://localhost:3000/parks')
-//     .then((results) => {
-//       this.setState({parkData: results.data});
-//     }).catch((err) => {
-//       console.log(err);
-//     })
-//   }
-
-
-//   render() {
-//     let parkData = Object.entries(this.state.parkData);
-//     return (
-//       <div className="main">
-//         <h1>Park Pack</h1>
-//         {parkData.map(park => {
-//           <div>
-//             <h3>park.name</h3>
-//             <TimeList></TimeList>
-//           </div>
-//         })}
-//       </div>
-//     );
-
-//   }
-// }
 
 
 export default function App() {
