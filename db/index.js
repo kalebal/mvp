339605a345df36e44d2501bebed190802ff09ac4
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-31 15:58:11 -0400

    use firebase

--- db/index.js
+++ db/index.js
@@ -1,40 +1,12 @@
-const mongo = require('mongodb');
-
-const MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-var dbName = 'dogparks';
-
-let _database;
+const firebase = require('firebase/app');
+const firestore = require('firebase/firestore');
+const { firebaseConfig } = require('../db.config.js');
 
 module.exports = {
-  connect(callback) {
-    MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
-      if (err) {
-        console.log('IN DBUTIL ERROR CONNECTING TO DB');
-        console.log(err);
-        return callback(err);
-      }
-      _database = client.db(dbName);
-      return callback(err, client);
-    });
+  connect() {
+    return firebase.initializeApp(firebaseConfig);
   },
-
   getDatabase() {
-    return _database;
-  },
-
-  getNextSequence(name = 'parkid') {
-    console.log('get next seq');
-
-    return _database.collection('counters').findOneAndUpdate(
-      { _id: name},
-      { $inc: { seq: 1 }},
-      { returnNewDocument: true }
-
-    ).then((newDoc) => {
-      console.log('new doc', newDoc);
-      return newDoc.value.seq;
-    });
+    return firebase.firestore();
   }
-};
-
+}
