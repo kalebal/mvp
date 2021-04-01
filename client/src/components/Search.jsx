276cfc79520dde1ed8a45a23fab1eb1874c7c8ae
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-01 10:50:40 -0400

    init search component

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import useInput from './useInput.jsx';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function Search() {
+  const { inputs, handleChange, handleSubmit } = useInput(search);
+
+  const search = () => {
+    console.log('search');
+  }
+  return (
+    <div className="form">
+      <h3>Find A Park</h3>
+      <div>
+        <label>Search:</label>
+        <input
+          onChange={handleChange}
+          value={inputs.name}
+          type="text"
+          name="search"
+          id="search"></input>
+      </div>
+    </div>
+  );
+
+}
