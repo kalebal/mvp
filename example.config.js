dd16ce735c1860a2fa0bf47ac4e5990772c03fc8
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-01 10:48:09 -0400

    Example config

---
+++ example.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "APIKEY",
+  authDomain: "AUTH",
+  databaseURL: "URL",
+  projectId: "PROJID",
+  storageBucket: "STORAGE",
+  messagingSenderId: "ID",
+  appId: "APPID"
+};
